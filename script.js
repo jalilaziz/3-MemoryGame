eaf9664f6ca6d017a69dc0ecdeec52d6e8a22e93
jalilaziz <jaliilaziiz@gmail.com>
2023-11-02 22:39:48 +0100

    add changes

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ let windo = document.querySelectorAll(".window");
           e.target.classList.toggle("active");
         });
       });
+
+//decoration
+var rn = Math.floor((Math.random() * 150) + 60);
+var rs = Math.floor((Math.random() * 11) + 4);
+  var t = new Trianglify({
+ x_gradient: Trianglify.colorbrewer.Spectral[rs],
+    noiseIntensity: 0,
+    cellsize: rn
+});
+var pattern = t.generate(window.innerWidth, window.innerWidth+200);
+document.body.setAttribute('style', 'background-image: '+pattern.dataUrl);
