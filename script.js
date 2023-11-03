175505739e7d52d29a4f6e83c38e8b65a32b3fef
jalilaziz <jaliilaziiz@gmail.com>
2023-11-03 17:01:35 +0100

    add changes

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 let windo = document.querySelectorAll(".window");
-      windo.forEach((element) => {
-        element.addEventListener("click", (e) => {
-          e.target.classList.toggle("active");
-        });
-      });
-
-//decoration
-var rn = Math.floor((Math.random() * 150) + 60);
-var rs = Math.floor((Math.random() * 11) + 4);
-  var t = new Trianglify({
- x_gradient: Trianglify.colorbrewer.Spectral[rs],
-    noiseIntensity: 0,
-    cellsize: rn
+windo.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    e.target.classList.toggle("active");
+  });
 });
-var pattern = t.generate(window.innerWidth, window.innerWidth+200);
-document.body.setAttribute('style', 'background-image: '+pattern.dataUrl);
+
+
+// //decoration
+// var rn = Math.floor((Math.random() * 150) + 60);
+// var rs = Math.floor((Math.random() * 11) + 4);
+//   var t = new Trianglify({
+//  x_gradient: Trianglify.colorbrewer.Spectral[rs],
+//     noiseIntensity: 0,
+//     cellsize: rn
+// });
+// var pattern = t.generate(window.innerWidth, window.innerWidth+200);
+// document.body.setAttribute('style', 'background-image: '+pattern.dataUrl);
